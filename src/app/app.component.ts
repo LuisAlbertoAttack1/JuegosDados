0570b2e508b78ab41c2bea564dc78d26d1dd7450
LuisAlbertoAttack1 <laaolivares837@gmail.com>
2023-04-25 11:59:25 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'juegos_dados';
+
+//variables que recogen los valores
+public valor1 : number = 0;
+public valor2 : number = 0;
+public valor3 : number = 0;
+public resultado : string = "";
+
+
+//Funcion para agregar un numero aleatorio
+  public numeroAleatorio() : number {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+
+//Constructor necesario
+  public constructor() {
+    this.valor1 = this.numeroAleatorio();
+    this.valor2 = this.numeroAleatorio();
+    this.valor3 = this.numeroAleatorio();
+  }
+
+//Funcion para poder tirar el dado
+  public tirar() : void {
+    this.valor1 = this.numeroAleatorio();
+    this.valor2 = this.numeroAleatorio();
+    this.valor3 = this.numeroAleatorio();
+    if (this.valor1 == this.valor2 && this.valor1 == this.valor3) {
+      this.resultado = 'Gano';
+    } else {
+      this.resultado = 'Perdio';
+    }
+  }
+
 }
---
+++ src/app/dado/dado.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dado',
+  templateUrl: './dado.component.html',
+  styleUrls: ['./dado.component.css']
+})
+export class DadoComponent {
+
+//Decorador de input
+  @Input() valor: number  = 0;
+
+}
+
